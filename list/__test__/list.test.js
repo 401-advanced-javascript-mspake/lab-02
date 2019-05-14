dc696002290c07c9fb763f0a7edc00bc96a175e2
Morgana Spake <morgana.spake@gmail.com>
2019-05-14 16:53:40 -0700

    pop tests added and passing

--- list/__test__/list.test.js
+++ list/__test__/list.test.js
@@ -1,10 +1,19 @@
 'use strict';
 
 // These 2 should be interchangeable!
-const List = require('../list.js');
-// const List = require('../list-constructor.js');
+// const List = require('../list.js');
+const List = require('../list-constructor.js');
+
 
 describe('List Data Structure', () => {
+  function prepopulatedStuff() {
+    let newList = new List();
+    newList.push('a');
+    newList.push('b');
+    newList.push('c');
+    return newList;
+  }
+
 
   it('starts with a length of 0 and an empty data set', () => {
     let stuff = new List();
@@ -18,6 +27,16 @@ describe('List Data Structure', () => {
     stuff.push('b');
     expect(stuff.length).toEqual(2);
     expect(stuff.data[1]).toEqual('b');
+  });
+
+  it('pops the last element off the end of the data set', () => {
+    let toPop = prepopulatedStuff();
+    toPop.pop();
+    expect(toPop.length).toEqual(2);
+  });
+
+  it('poping off an element returns the value of that element', () => {
+    expect(prepopulatedStuff().pop()).toEqual('c');
   });
 
 });

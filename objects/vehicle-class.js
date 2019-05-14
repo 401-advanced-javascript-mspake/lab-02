0b89bd02172da96ff4c8595e6dc4997a1e5444ab
Morgana Spake <morgana.spake@gmail.com>
2019-05-14 15:25:41 -0700

    vehicle as class added, tests passing

--- objects/vehicle-class.js
+++ objects/vehicle-class.js
@@ -1,6 +1,69 @@
 'use strict';
 
-class Car {
+class Vehicle {
+  constructor(name, wheels) {
+    this.name = name;
+    this.wheels = wheels;
+  }
+
+  drive() {
+    return 'Moving Foreward';
+  }
+
+  stop() {
+    return 'Stopping';
+  }
 }
 
-module.exports = Car;
+class Car extends Vehicle {
+  constructor(name) {
+    super(name);
+    this.wheels = 4;
+  }
+}
+
+class Motorcycle extends Vehicle {
+  constructor(name, wheels) {
+    super(name);
+    this.wheels = 2;
+  }
+
+  wheelie() {
+    return 'Wheee!'
+  }
+}
+
+module.exports = {Car, Motorcycle};
+
+/* 
+const Vehicle = function(name, wheels) {
+  this.name = name;
+  this.wheels = wheels;
+};
+
+Vehicle.prototype.drive = () => {
+  return 'Moving Forward';
+};
+
+Vehicle.prototype.stop = () => {
+  return 'Stopping';
+};
+
+// Car Constructor
+const Car = function(name) {
+  Vehicle.call(this, name, 4);
+};
+
+Car.prototype = new Vehicle();
+
+const Motorcycle = function(name) {
+  Vehicle.call(this,name,2);
+};
+
+Motorcycle.prototype = new Vehicle();
+
+Motorcycle.prototype.wheelie = () => {
+  return 'Wheee!';
+};
+
+*/
